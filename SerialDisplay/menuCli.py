896cf57120845499d6573e419a71cf34cfe2e53d
geraskalnas <geraskalnas@gmail.com>
2019-04-13 13:26:29 +0300

    Enabled passing a function with anrgs and ...

--- SerialDisplay/menuCli.py
+++ SerialDisplay/menuCli.py
@@ -4,13 +4,13 @@ class menu():
     def truncate(self, n, decimals=0):
         multiplier = 10 ** decimals
         return int(n * multiplier) / multiplier
-    def addItem(self, name, action):
-        self.menuL.append([name, action])
+    def addItem(self, name, action, *args, **kwargs):
+        self.menuL.append([name, action, args, kwargs])
         return len(self.menuL)-1
     def removeItem(self, idd):
         del self.menuL[idd]
     def execute(self, idd):
-        self.menuL[idd][1]()
+        self.menuL[idd][1](*self.menuL[idd][2], **self.menuL[idd][3])
     def listItems(self, selected=0, limit=2):
         page=self.truncate(selected/limit)+1
         for i in range(page*limit-limit, page*limit):
